87a6310070dcf38b59401c923dfc839f71cbe741
Nabeel <42202507+NabeelFarooq@users.noreply.github.com>
2025-02-16 09:32:36 +0530

    Add a tree class having constructor and buildTree functions

--- balanced-binary-tree.js
+++ balanced-binary-tree.js
@@ -5,3 +5,19 @@ class node {
     this.right = right;
   }
 }
+
+class tree {
+  constructor(inputArray) {
+    this.root = this.buildTree(this.inputArray, 0, this.inputArray.length - 1);
+    prettyPrint(this.root);
+  }
+
+  buildTree(inputArray, start, end) {
+    if (start > end) return null;
+    let mid = parseInt((start + end) / 2);
+    let root = new node(inputArray[mid]);
+    root.left = this.buildTree(inputArray, start, mid - 1);
+    root.right = this.buildTree(inputArray, mid + 1, end);
+    return root;
+  }
+}
